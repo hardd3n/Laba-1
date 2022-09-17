6615be047a48df20bd09f1ff331d4be833a64b05
hardd3n <113800444+hardd3n@users.noreply.github.com>
2022-09-17 23:18:21 +0300

    Задание 1

---
+++ Задание 1 Гена.py
@@ -0,0 +1,11 @@
+one = 15
+two = 'слово'
+three = "два слова"
+four = [1, 2, 3, 4, 5]
+four.append(6)
+five = (1, 2, 3, 4, 5)
+print(15 / 3)
+print(two[2])
+print(three[0:3])
+print(four)
+print(five[2])
